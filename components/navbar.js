9eac3efef534e7432befbfd962b4550458f66d98
Alexandre Claveau <a.claveau29@gmail.com>
2021-02-02 22:00:01 +0100

    Ajout du menu via un composant externe

--- components/navbar.js
+++ components/navbar.js
@@ -2,11 +2,14 @@ import React from "react";
 import styles from "../styles/Navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
-import {useRouter} from "next/router";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useRouter } from "next/router";
+// import { AiOutlineMenu } from "react-icons/ai";
+import { Menu, MenuItem, MenuButton } from "@szhsin/react-menu";
+import "@szhsin/react-menu/dist/index.css";
 
 export const Navbar = () => {
   const router = useRouter();
+
   return (
     <nav className={styles.container}>
       <Link href="/">
@@ -22,21 +25,55 @@ export const Navbar = () => {
       </Link>
       <ul className={styles.links}>
         <li>
-          <a href={router.pathname === "/contact" ? "/#about" : "#about"}>A propos</a>
+          <a href={router.pathname === "/contact" ? "/#about" : "#about"}>
+            A propos
+          </a>
         </li>
         <li>
-          <a href={router.pathname === "/contact" ? "/#skills" : "#skills"}>Compétences</a>
+          <a href={router.pathname === "/contact" ? "/#skills" : "#skills"}>
+            Compétences
+          </a>
         </li>
         <li>
-          <a href={router.pathname === "/contact" ? "/#experiences" : "#experiences"}>Expériences</a>
+          <a
+            href={
+              router.pathname === "/contact" ? "/#experiences" : "#experiences"
+            }
+          >
+            Expériences
+          </a>
         </li>
       </ul>
-      <div className={styles.menu}>
+      <Menu menuButton={<MenuButton>Menu</MenuButton>}>
+        <MenuItem>
+          <a href={router.pathname === "/contact" ? "/#about" : "#about"}>
+            A propos
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a href={router.pathname === "/contact" ? "/#skills" : "#skills"}>
+            Compétences
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a
+            href={
+              router.pathname === "/contact" ? "/#experiences" : "#experiences"
+            }
+          >
+            Expériences
+          </a>
+        </MenuItem>
+        <MenuItem>
+          <a href="/contact">Contact</a>
+        </MenuItem>
+      </Menu>
+      {/* <div className={styles.menuButton}>
         <AiOutlineMenu />
-      </div>
+      </div> */}
       <Link href="/contact">
         <a className={styles.contact}>Me contacter</a>
       </Link>
     </nav>
   );
-}
+};
